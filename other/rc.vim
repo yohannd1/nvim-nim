e58a94802acd6b7fb8ae8a70b32408c0a560cd3b
baabelfish <baabelfish@gmail.com>
2016-03-14 19:04:17 +0200

    Test new test framework

---
+++ other/rc.vim
@@ -0,0 +1,5 @@
+filetype off
+set rtp+=./plugins/vader.vim
+set rtp+=./plugins/nvim-nim
+filetype plugin indent on
+syntax enable
--- other/run_tests.sh
+++ other/run_tests.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-export NVIM_LISTEN_ADDRESS="/tmp/nvim"
-nvim --headless&
-nvimpid=$!
-sleep 2
-sh -c "py.test -v tests/vim/**.py"
-haderr=$?
-sleep 1
-kill $nvimpid
-wait
-exit $haderr
+
+# Intall plugins
+mkdir plugins
+git clone https://github.com/baabelfish/vader.vim plugins/vader.vim
+git clone https://github.com/baabelfish/nvim-nim plugins/nvim-nim
+
+# Run tests
+nvim -u rc.vim -c 'Vader! tests/**/*.vader'
+err=$?
+if [ "$err" != "0" ]; then
+    cat report.log
+    exit 1
+fi
